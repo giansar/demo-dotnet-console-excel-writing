01a1bb5a44ee9cce4becc8152dc7513e928bc9e0
Giansar Pratiknya <gpratiknya@gmail.com>
2021-02-13 20:33:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using ClosedXML.Excel;
+
+namespace demo_dotnet_console_excel_writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //reading configuration
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+            String connectionString = configuration.GetConnectionString("postgresql");
+
+            Task<List<Province>> provinces = GetDataFromDb(connectionString);
+
+            using (IXLWorkbook wb = new XLWorkbook())
+            {
+                IXLWorksheet ws = wb.Worksheets.Add("Province");
+                int row = 1;
+                provinces.Result.ForEach(province =>
+                {
+                    Console.WriteLine(province.id + " - " + province.name);
+                    int column = 1;
+                    ws.Cell(row, column++).Value = province.id;
+                    ws.Cell(row++, column).Value = province.name;
+                });
+                wb.SaveAs("helo.xlsx");
+            }
+        }
+
+        public static async Task<List<Province>> GetDataFromDb(String connectionString)
+        {
+            List<Province> provinces = new List<Province>();
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Postgresql version = " + connection.ServerVersion);
+                    String sql = "SELECT * FROM province";
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                    {
+                        NpgsqlDataReader dataReader = await command.ExecuteReaderAsync();
+                        while (dataReader.Read())
+                        {
+                            provinces.Add(new Province(Convert.ToInt32(dataReader[0]), Convert.ToString(dataReader[1])));
+                        }
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return provinces;
+        }
+    }
+}
---
+++ Province.cs
@@ -0,0 +1,14 @@
+namespace demo_dotnet_console_excel_writing
+{
+    public class Province
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Province(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
